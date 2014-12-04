247b8a37b3e94d70eef0946bd99f42c2dc2db0e0
David Hart <dbhart@sandia.gov>
2014-12-04 11:05:35 -0700

    Minor Changes

--- lfw/classifyImages.m
+++ lfw/classifyImages.m
@@ -3,22 +3,23 @@
 % Math 415 Project 4
 % Christopher K., Darrel B.
 function [meanVals, stdVals, vals] = classifyImages(MCmax)
-  img = imread('0.jpg');
+  img = rgb2gray(imread('1.jpg'));
   [m,n] = size(img);
   gender=load('gender.txt');
-  nimg = 1000;
+  nimg = 200;
+  gender = gender(1:nimg);
   if nargin<1,
-    MCmax = 10;
+    MCmax = 1;
   end
-  tSizes = 250;% [5 10 15 25 50];% 100 200];
-  nKs = [5 10 15 25 50];%[5 10 15 25 50];
+  tSizes = [5 10 15 25 50];% [5 10 15 25 50];% 100 200];
+  nKs = [5 50];%[5 10 15 25 50];
   NTmax = length(tSizes);
   NKmax = length(nKs);
   allImgs = zeros(m*n,nimg);
   allSmile = zeros(m*n,nimg);
   for i = 1:nimg
     filename = sprintf('%d.jpg',i);
-    img = imread(filename);
+    img = rgb2gray(imread(filename));
     allImgs(:,i) = img(:);
   end
   meanImg = mean(allImgs,2);
@@ -35,6 +36,8 @@ function [meanVals, stdVals, vals] = classifyImages(MCmax)
   vals = zeros(MCmax*NTmax*NKmax,16);
   meanVals = zeros(NTmax*NKmax,16);
   stdVals = zeros(NTmax*NKmax,16);
+  nMale = sum(gender==1);
+  nFemale = sum(gender==-1);
   
   %% MC Loop
   k = 0;
@@ -45,8 +48,13 @@ function [meanVals, stdVals, vals] = classifyImages(MCmax)
       for M = 1:MCmax
         doClassification();
       end
-      meanVals(kt,:) = nanmean(vals(MCmax*(kt-1)+1:MCmax*kt,:));
-      stdVals(kt,:) = nanstd(vals(MCmax*(kt-1)+1:MCmax*kt,:));      
+      if MCmax > 1,
+        meanVals(kt,:) = nanmean(vals(MCmax*(kt-1)+1:MCmax*kt,:));
+        stdVals(kt,:) = nanstd(vals(MCmax*(kt-1)+1:MCmax*kt,:));
+      else
+        meanVals(kt,:) = vals(MCmax*(kt-1)+1:MCmax*kt,:);
+        stdVals(kt,:) = vals(MCmax*(kt-1)+1:MCmax*kt,:);
+      end
     end
   end
   meanVals = meanVals(1:kt,:);
@@ -56,7 +64,7 @@ function [meanVals, stdVals, vals] = classifyImages(MCmax)
   function doClassification()
     %% Training dataset info
     k = k+1;
-    trainSet = randperm(200,nTrain);
+    trainSet = randperm(nimg,nTrain);
     trainGend = gender(trainSet);
     trainImgs = allImgs(:,trainSet);
 %     trainMean = mean(trainImgs,2);
@@ -85,7 +93,7 @@ function [meanVals, stdVals, vals] = classifyImages(MCmax)
     MasF = sum(diff(gender==1)~=0);
     FasM = sum(diff(gender==-1)~=0);
     FasF = sum(diff(gender==-1)==0);
-    vals(k,5:8) = [MasM, FasM, MasF, FasF]./nimg;
+    vals(k,5:8) = [MasM, FasM, MasF, FasF];
     disp('numTrainImg  numKValues');
     disp([nTrain,nK]);
     table_NoSmileRecogNoSmile = [MasM, FasM;MasF, FasF];
@@ -101,41 +109,13 @@ function [meanVals, stdVals, vals] = classifyImages(MCmax)
     FMR = (FM)/(FF+FM);
     FFR = (FF)/(TM+FF);
     TFR = (TF)/(TF+FM);
+    vals(k,5:8) = [TMR, FMR, FFR, TFR];
 %     MPV = (TM)/(TM+FM);
 %     FPV = (TF)/(TF+FF);
     LRp = TMR/FMR;
     LRn = FFR/TFR;
     DOR = LRp/LRn;
     vals(k,9:10) = [ACC,DOR];
-    
-    
-    %% Nonsmiling to smiling
-    [smileCl] = classify(smileV(1:nK,:)',V(:,1:nK),trainGend,'diaglinear');
-    diff = smileCl - gender;
-    MasM = sum(diff(gender==1)==0);
-    MasF = sum(diff(gender==1)~=0);
-    FasM = sum(diff(gender==-1)~=0);
-    FasF = sum(diff(gender==-1)==0);
-    vals(k,11:14) = [MasM, FasM, MasF, FasF]./nimg;
-    table_NoSmileRecogSmile = [MasM, FasM;MasF, FasF];
-    disp(table_NoSmileRecogSmile);
-    TM = MasM;
-    TF = FasF;
-    FM = MasF;
-    FF = FasM;
-    ACC = (TM + TF)/nimg;
-%     FDR = (FM)/(TM+FM);
-%     FOR = (FF)/(TF+FF);
-    TMR = (TM)/(TM+FF);
-    FMR = (FM)/(FF+FM);
-    FFR = (FF)/(TM+FF);
-    TFR = (TF)/(TF+FM);
-%     MPV = (TM)/(TM+FM);
-%     FPV = (TF)/(TF+FF);
-    LRp = TMR/FMR;
-    LRn = FFR/TFR;
-    DOR = LRp/LRn;
-    vals(k,15:16) = [ACC,DOR];
     
     % tmp = U*S*V(15,:)';
     % figure(4)
